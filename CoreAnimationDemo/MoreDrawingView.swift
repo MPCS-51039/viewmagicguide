1c997b00560612e9caa057570e8a0fd93fbef4e3
Chelsea Troy <chelsea.dommert@gmail.com>
2019-04-21 18:24:38 -0500

    Start Drawing in Drawing View

--- CoreAnimationDemo/MoreDrawingView.swift
+++ CoreAnimationDemo/MoreDrawingView.swift
@@ -16,7 +16,21 @@ class MoreDrawingView: UIView {
     */
     
     override func draw(_ rect: CGRect) {
-        // Drawing code
+        let width = rect.width
+        let height = rect.height
+        
+        let lines = UIBezierPath()
+        
+        lines.move(to: CGPoint(x: 0.0, y: height / 4))
+        lines.addLine(to: CGPoint(x: width, y: height / 4))
+        
+        lines.move(to: CGPoint(x: 0.0, y: 3 * height / 4))
+        lines.addLine(to: CGPoint(x: width, y: 3 * height / 4))
+
+        lines.setLineDash([10, 5], count: 2, phase: 0)
+        lines.lineWidth = 5
+        lines.stroke()
+        
     }
 
 
